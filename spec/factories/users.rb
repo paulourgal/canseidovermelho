ca783e1c2a975639afd03b7e299d136561dfc50c
Paulo Urgal <paulo@caiena.net>
2015-03-14 16:00:16 -0300

    close #4: [+] novos atributos para o model user; [+] validações no model user;

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,21 @@ FactoryGirl.define do
 
   factory :user do
 
-    name "Teste"
     birth_date Date.today
+    confirmed false
     sequence(:email) { |n| "test#{n}@example.com" }
+    name "Teste"
     password "s3cr37"
     password_confirmation "s3cr37"
+    role User::ROLES.index(:admin)
+
+    trait :user do
+      role User::ROLES.index(:user)
+    end
+
+    trait :confirmed do
+      confirmed true
+    end
 
     trait :invalid do
       password nil
