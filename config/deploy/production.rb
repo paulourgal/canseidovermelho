fc331c55896a7618aea3837af068233bdaaf4fa9
Paulo Urgal <paulo.urgal@gmail.com>
2016-05-31 06:20:22 -0300

    [*] back to ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rvm_type, :system
-set :rvm_ruby_version, '2.2.4@canseidovermelho'
+set :rvm_ruby_version, '2.1.1@canseidovermelho'
 
 server "canseidovermelho.com", user: "canseidovermelho", roles: %w{web app db}
