47860fc2b5bd89f929c86e9dc19d60cfb2e1dd5f
Paulo Urgal <paulo.urgal@gmail.com>
2016-06-09 07:29:15 -0300

    [*] fix money field problem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def currency(value)
+    number_with_precision(value.to_f, precision: 2)
+  end
+
 end
--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -1,4 +1,5 @@
 class SaleItem < ActiveRecord::Base
+  include Monetizable
 
   # associations
 
@@ -9,5 +10,10 @@ class SaleItem < ActiveRecord::Base
 
   validates :item_id, presence: true
   validates :price, presence: true
+
+  def price=(money)
+    unmasked = unmask_currency(money) if money.present?
+    self[:price] = unmasked
+  end
 
 end
